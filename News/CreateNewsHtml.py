02d90831c81a143adfaf2047d32f1bd6cc7685b3
Seis Bolado <seisbolado@gmail.com>
2021-12-31 02:13:27 -0300

    Few changes

--- News/CreateNewsHtml.py
+++ News/CreateNewsHtml.py
@@ -6,9 +6,10 @@ class CreateNewsHtml:
     def __init__(self, html, title, text):
         self.html = html
         self.title = title
-        GetAudioFile(title, text)
+        GetAudioFile(str(title).replace(" ", "-").replace("'", "").replace('"', "").replace("|", "").replace(":", ""), text)
         self.SaveHtml()
     def SaveHtml(self):
-        self.file = open("c:/xampp/htdocs/jornaut/news/"+str(self.title).replace(" ", "-").replace("'", "").replace('"', "")+".php","w", encoding='utf-8')
+        self.file = open("c:/xampp/htdocs/jornaut/news/"+str(self.title).replace(" ", "-").replace("'", "").replace('"', "").replace(":", "")+".php","w", encoding='utf-8')
         self.file.write("<?php include '../header.php'?>"+str(self.title)+"<?php include '../middleA.php'?>"+"<embed height='50' width='100' src='c:/xampp/htdocs/jornaut/adds/"+str(self.title).replace(" ", "-").replace("'", "").replace('"', "")+".wav'>"+"<? include '../middleB.php'?>"+self.html+"<? include '../footer.php'?>")
+        self.response = "<?php include '../header.php'?>"+str(self.title)+" OpEd"+"<?php include '../middle.php'?>"+str(self.html)+"<? include '../footer.php'?>"
         self.file.close()
