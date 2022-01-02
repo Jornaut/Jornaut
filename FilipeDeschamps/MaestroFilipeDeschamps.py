8cd9b0516c7172840269fc80228b5c7449a2230d
Seis Bolado <seisbolado@gmail.com>
2022-01-02 03:08:54 -0300

    uups

--- FilipeDeschamps/MaestroFilipeDeschamps.py
+++ FilipeDeschamps/MaestroFilipeDeschamps.py
@@ -11,12 +11,12 @@ class MaestroFilipeDeschamps:
         self.list = GetEmails().response
         os.mkdir("c:/xampp/htdocs/Jornaut/FilipeDeschamps/"+str(time))
         if datetime.date.today().weekday() < 5:
-        for FilipeDeschamps in self.list:
-            if not self.list[self.list.index(FilipeDeschamps)] in [self.list[0], self.list[-1]]:
-                FilipeDeschamps = str(FilipeDeschamps).replace('''\r''', "").replace('''\n''', "")
-                self.text = FilipeDeschamps
-                self.html = CreateHtml(FilipeDeschamps)
-                self.theme = self.html.theme
-                self.html = AddFilipeDeschampsImages(self.text, self.html.response)
-                CreateFilipeDeschampsHtml(self.html.response, self.theme, self.text, time)
-                # self.ConcAud.append(['c:/xampp/htdocs/jornaut/adds/'+str(theme).replace(" ", "-").replace("'", "").replace('"', "").replace("|", "").replace(":", "")+"OpEd"+'.wav', self.html.img])
+            for FilipeDeschamps in self.list:
+                if not self.list[self.list.index(FilipeDeschamps)] in [self.list[0], self.list[-1]]:
+                    FilipeDeschamps = str(FilipeDeschamps).replace('''\r''', "").replace('''\n''', "")
+                    self.text = FilipeDeschamps
+                    self.html = CreateHtml(FilipeDeschamps)
+                    self.theme = self.html.theme
+                    self.html = AddFilipeDeschampsImages(self.text, self.html.response)
+                    CreateFilipeDeschampsHtml(self.html.response, self.theme, self.text, time)
+                    # self.ConcAud.append(['c:/xampp/htdocs/jornaut/adds/'+str(theme).replace(" ", "-").replace("'", "").replace('"', "").replace("|", "").replace(":", "")+"OpEd"+'.wav', self.html.img])
